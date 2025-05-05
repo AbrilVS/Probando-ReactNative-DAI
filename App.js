71310364358c13559df84455657fa920ce351c5f
ABRIL VARELA SICA <48464274@ort>
2025-05-05 08:54:09 -0300

    ahora si

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Vibration
+} from 'react-native';
+
 
 export default function App() {
+  const [contador,setContador] = useState(0);
+  const sumar = () => {
+   
+    setContador(contador+1);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>MARMOTAAAAAAAAAAAAAAAAAAAAA</Text>
+      <Image
+        source={require('./assets/marmota.jpg')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>{contador}</Text>
+      <Button
+        title="sumar"
+        onPress={()=> setContador(contador+1)}
+        color="green"   // <-- botón en verde
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',  // fondo rojo
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    color: '#fff',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 30,
   },
 });
--- index.js
+++ index.js
@@ -1,61 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import { registerRootComponent } from 'expo';
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  Vibration
-} from 'react-native';
+import App from './App';
 
-
-export default function App() {
-  const [contador,setContador] = useState(0);
-  const sumar = () => {
-   
-    setContador(contador+1);
-  };
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MARMOTAAAAAAAAAAAAAAAAAAAAA</Text>
-      <Image
-        source={require('./assets/marmota.jpg')}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{contador}</Text>
-      <Button
-        title="suamr"
-        onPress={()=> setContador(contador+1)}
-        color="green"   // <-- botón en verde
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',  // fondo rojo
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 18,
-    color: '#fff',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  image: {
-    width: 200,
-    height: 200,
-    resizeMode: 'contain',
-    marginBottom: 30,
-  },
-});
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
